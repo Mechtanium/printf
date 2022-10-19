0961c3b981343de4db39561aef4a332f3d3415ff
Chibueze Igbokwe <igbokwechibuezeo@gmail.com>
2022-10-19 04:20:35 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
 #define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
+
 int _putchar(char c); /* For testing. Don't delete */
 int _printf(const char *format, ...);
 void format_writter(const char *format, int *n, va_list ls);
